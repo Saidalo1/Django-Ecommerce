186b239e750bcaaf80ba8e0b2491ef82945e6995
Saidalo1 <super.saydalo@mail.ru>
2023-02-02 09:45:11 +0500

    Add forgot password

---
+++ users/views/forgot_password.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+
+from users.mixins import AuthUserMixin
+
+
+class ForgotPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'auth/forgot_password.html'
+    email_template_name = "auth/messages/forgot_password_email.html"
+    success_message = 'Password reset instruction message has sent to your email!'
+    success_url = reverse_lazy('password_reset')
+
+
+class ForgotPasswordConfirmView(SuccessMessageMixin, AuthUserMixin, PasswordResetConfirmView):
+    template_name = 'auth/forgot_password_confirm.html'
+    success_message = 'Password has been changed!'
+    success_url = reverse_lazy('login')
